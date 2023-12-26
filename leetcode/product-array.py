d543cf27ff8ef602cf69157d5f75f7f0e7331239
Stephanie Kariuki <110028520+stephkariuki19@users.noreply.github.com>
2023-12-26 21:31:41 +0300

    Create product-array.py
    Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[I]. eg Input: nums = [1,2,3,4]
    Output: [24,12,8,6]

---
+++ leetcode/product-array.py
@@ -0,0 +1,12 @@
+PRODUCT OF ARRAY EXCEPT SELF
+class Solution(object):
+    def productExceptSelf(self, nums):
+        output = []
+        for i in range(len(nums)):
+            skip_index = i
+            product = 1  # Reset product for each i
+            for j, num in enumerate(nums):  # Use j instead of i to avoid conflict
+                if j != skip_index:
+                    product *= num
+            output.append(product)
+        return output
