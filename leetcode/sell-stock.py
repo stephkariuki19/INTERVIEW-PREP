8f3ef3ba2d4287e94e62039cd26a5b8ff87176da
Stephanie Kariuki <110028520+stephkariuki19@users.noreply.github.com>
2024-01-12 10:30:38 +0300

    Update sell-stock.py

--- leetcode/sell-stock.py
+++ leetcode/sell-stock.py
@@ -1,13 +1,12 @@
 class Solution(object):
-    def maxProfit(self, prices):
-     
-        max_diff = 0
-        for i in range(len(prices)-1,-1,-1):
-            for j in range(i-1,-1,-1):
-                diff = prices[i]- prices[j] 
-                if (diff > max_diff):
-                    max_diff = diff
-               
-            
-        return max_diff
-Solution().maxProfit([7,1,5,3,6,4])
+    def maxProfit(self, nums):
+        min_price = float("inf")
+        profit = 0
+
+        for i in range(len(nums)):
+            if nums[i] < min_price:
+                min_price = nums[i]
+            elif profit < nums[i] - min_price:
+                profit = nums[i] - min_price
+
+        return profit
